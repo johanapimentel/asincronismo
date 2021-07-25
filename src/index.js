9bb519fcc99b233cb194306bd35649214b86e8df
Vanessa Pimentel <johanapimentel@gmail.com>
2021-07-25 04:54:02 -0500

    agregando cambios

---
+++ src/index.js
@@ -0,0 +1,39 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+//callback
+
+const sum = (num1, num2) => {
+    return num2 + num1;
+}
+
+const rest = (num1, num2) => {
+    return num2 - num1;
+}
+
+const calc = (num1, num2, callback) => {
+    return callback(num1, num2);
+}
+
+console.log(`suma: `, calc(2,2,sum));
+console.log(`resta: `, calc(3,2,rest));
+
+const date = (callback) => {
+    // console.log(new Date);
+    setTimeout(() => {
+        let date = new Date;
+        callback(date);
+    }, 3000);
+}
+
+const printDate = (dateNow) => {
+    console.log(`callback: `, dateNow);
+}
+
+date(printDate);
+
+// challenge request
+
+const fechtData = (url_api, callback) =>{
+    let xhttp = XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+}
